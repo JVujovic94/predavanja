960385c19468eea55bb9284ea97355a4438e237b
Jovana Vujovic <jovanavujovic94@gmail.com>
2021-12-28 19:32:23 +0100

    Print numbers to 10000

--- src/p28_12_2021/UvodPredavanja.java
+++ src/p28_12_2021/UvodPredavanja.java
@@ -4,7 +4,7 @@ public class UvodPredavanja {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 10000; i++) {
 			System.out.print(i + ", ");
 		}
 	}
